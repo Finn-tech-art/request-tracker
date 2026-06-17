8ea534a8d5b5d68ea4f0f7fc7685006705c83cff
Finnley <finnleybaraka89@gmail.com>
2026-06-17 21:30:42 +0300

    Add the requestsModedl

---
+++ src/js/models/requestModel.js
@@ -0,0 +1,24 @@
+export default class Request {
+    constructor({
+        id,
+        name,
+        email,
+        product,
+        requesType,
+        priority,
+        message,
+        status = "Open",
+        createdAt = new Date().toISOString()
+    }) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.product = product;
+        this.requestType = requestType;
+        this.priority = priority;
+        this.message = message;
+        this.status = status;
+        this.createdAt = createdAt;
+
+    }
+}
